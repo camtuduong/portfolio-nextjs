e66086f398c302e22dc1765f69ddf758b3dc230b
CamtuDuong <camtu28082003@gmail.com>
2026-03-22 13:57:30 +0000

    chore/setup-folders-clsx

---
+++ src/views/Projects/index.tsx
@@ -0,0 +1,16 @@
+import Dash from "@/icons/Dash";
+import React from "react";
+
+export default function Projects() {
+  return (
+    <div>
+      {/* Hero Section */}
+      <div className="flex flex-col gap-1 items-center">
+        <h2 className="text-5xl font-bold leading-[72px] font-playfair">
+          Projects
+        </h2>
+        <Dash />
+      </div>
+    </div>
+  );
+}
